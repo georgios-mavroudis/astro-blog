49fb2aeb2332c1fcadc5a83ab416abcaaf056cd7
georgios-mavroudis <georgios@cardiologs.com>
2023-09-25 19:05:55 +0200

    Added the node adapter for the hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [mdx()]
+  output: 'hybrid',
+  integrations: [mdx()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
